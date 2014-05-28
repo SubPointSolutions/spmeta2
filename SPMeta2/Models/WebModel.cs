92de665d4c348164d6343f56ce77f341125fdb9b
avishnyakov <anton.vishnyakov@gmail.com>
2014-05-28 18:50:31 +1000

    + added WebModels.cs

---
+++ SPMeta2/Models/WebModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPMeta2.Models
+{
+    public abstract class WebModel : ModelBase
+    {
+        #region constructors
+
+        #endregion
+
+        #region properties
+
+        #endregion
+
+
+        #region methods
+
+        public abstract ModelNode GetSiteModel();
+        public abstract ModelNode GetRootWebModel();
+        public abstract ModelNode GetWebModel();
+
+        #endregion
+    }
+}
