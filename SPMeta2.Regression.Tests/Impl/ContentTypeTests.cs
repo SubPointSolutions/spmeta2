b65f40e5ea1082211d2d5cb7e79665b050a4ec1e
avishnyakov <anton.vishnyakov@gmail.com>
2014-06-16 16:22:28 +1000

    + a few content type tests

--- SPMeta2.Regression.Tests/Impl/ContentTypeTests.cs
+++ SPMeta2.Regression.Tests/Impl/ContentTypeTests.cs
@@ -1,26 +1,34 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.Regression.Model.Definitions;
 using SPMeta2.Regression.Tests.Base;
+using SPMeta2.Syntax.Default;
 
 namespace SPMeta2.Regression.Tests.Impl
 {
-    //[TestClass]
+    [TestClass]
     public class ContentTypeTests : SPMeta2RegresionTestBase
     {
         #region tests
 
         [TestMethod]
         [TestCategory("Regression")]
-        public void CanDeployContentTypesWithoutFields()
+        public void CanDeployCustomDocumentContentType()
         {
-            throw new NotImplementedException();
+            var model = SPMeta2Model
+                .NewSiteModel(site => site.AddContentType(RegContentTypes.CustomDocument));
+
+            WithProvisionRunners(runner => runner.DeploySiteModel(model));
         }
 
         [TestMethod]
         [TestCategory("Regression")]
-        public void CanDeployContentTypesWithFieldRefs()
+        public void CanDeployCustomListItemContentType()
         {
-            throw new NotImplementedException();
+            var model = SPMeta2Model
+                .NewSiteModel(site => site.AddContentType(RegContentTypes.CustomItem));
+
+            WithProvisionRunners(runner => runner.DeploySiteModel(model));
         }
 
         [TestMethod]
