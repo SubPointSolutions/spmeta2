3f886c3ad4a18c6c7e83b917ea146346e16fc94b
avishnyakov <anton.vishnyakov@gmail.com>
2014-06-16 13:31:15 +1000

    + added SPMeta2.O365, v16 deps

---
+++ SPMeta2.O365/ModelHosts/O365ModelHostBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SharePoint.Client;
+using SPMeta2.ModelHosts;
+
+namespace SPMeta2.O365.ModelHosts
+{
+    public abstract class O365ModelHostBase : ModelHostBase
+    {
+        #region properties
+
+        public ClientContext HostClientContext { get; set; }
+
+        public Site HostSite { get; set; }
+        public Web HostWeb { get; set; }
+
+        #endregion
+    }
+}
---
+++ SPMeta2.O365/ModelHosts/SiteModelHost.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.SharePoint.Client;
+
+namespace SPMeta2.O365.ModelHosts
+{
+    public class SiteModelHost : O365ModelHostBase
+    {
+        #region constructors
+
+        public SiteModelHost()
+        {
+            
+        }
+
+        public SiteModelHost(ClientContext clientContext)
+        {
+            HostClientContext = clientContext;
+            HostSite = clientContext.Site;
+        }
+
+        #endregion
+
+        #region static
+
+        public static SiteModelHost FromClientContext(ClientContext clientContext)
+        {
+            return new SiteModelHost(clientContext);
+        }
+
+        #endregion
+    }
+}
