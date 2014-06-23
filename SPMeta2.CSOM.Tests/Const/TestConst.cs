ec30e87a60f875930b17a1e14d1c580696da710a
avishnyakov <anton.vishnyakov@gmail.com>
2014-06-23 14:09:48 +1000

    + solution cleanup

--- SPMeta2.CSOM.Tests/Const/TestConst.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SPMeta2.CSOM.Tests.Const
-{
-    public static class TestConst
-    {
-        public static string DefaultSharePointSiteUrl = string.Format("http://{0}/spmeta2-csharp-client-demo", Environment.MachineName);
-    }
-}
--- SPMeta2.SSOM.Samples.FoundationAppTests/Models/ModelConsts.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SPMeta2.SSOM.Samples.FoundationAppTests.Models
-{
-    public static class ModelConsts
-    {
-        public const string MetadataGroup = "FoundationApp";
-    }
-}
