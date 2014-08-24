356618426c0de702720a7aaeff1edb52f27d59a2
SubPointSupport <support@subpointsolutions.com>
2014-08-24 14:21:35 -0700

    CSOM BreakRoleInheritanceModelHandler stub

---
+++ SPMeta2/SPMeta2.CSOM/ModelHandlers/BreakRoleInheritanceModelHandler.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.SharePoint.Client;
+using SPMeta2.CSOM.ModelHosts;
+using SPMeta2.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMeta2.Utils;
+
+namespace SPMeta2.CSOM.ModelHandlers
+{
+    public class BreakRoleInheritanceModelHandler : CSOMModelHandlerBase
+    {
+        #region properties
+
+        public override Type TargetType
+        {
+            get { return typeof(BreakRoleInheritanceDefinition); }
+        }
+
+        #endregion
+
+        #region methods
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var securableObject = ExtractSecurableObject(modelHost);
+            var breakRoleInheritanceModel = model.WithAssertAndCast<BreakRoleInheritanceDefinition>("model", value => value.RequireNotNull());
+
+            ProcessRoleInheritance(modelHost, securableObject, breakRoleInheritanceModel);
+        }
+
+        private void ProcessRoleInheritance(object modelHost, SecurableObject securableObject, BreakRoleInheritanceDefinition breakRoleInheritanceModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        private SecurableObject ExtractSecurableObject(object modelHost)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
