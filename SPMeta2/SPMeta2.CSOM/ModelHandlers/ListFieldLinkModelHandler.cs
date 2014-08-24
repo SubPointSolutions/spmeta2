7a5ab6d4dd612d1713e61af51696f5ad97de627f
SubPointSupport <support@subpointsolutions.com>
2014-08-24 14:35:30 -0700

    added ListFieldLinkModelHandler CSOM stub

---
+++ SPMeta2/SPMeta2.CSOM/ModelHandlers/ListFieldLinkModelHandler.cs
@@ -0,0 +1,43 @@
+﻿using SPMeta2.CSOM.ModelHosts;
+using SPMeta2.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMeta2.Utils;
+using Microsoft.SharePoint.Client;
+
+namespace SPMeta2.CSOM.ModelHandlers
+{
+    public class ListFieldLinkModelHandler : CSOMModelHandlerBase
+    {
+        #region properties
+
+        public override Type TargetType
+        {
+            get { return typeof(ListFieldLinkDefinition); }
+        }
+ 
+        #endregion
+
+        #region methods
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var listModelHost = modelHost.WithAssertAndCast<ListModelHost>("modelHost", value => value.RequireNotNull());
+            var listFieldLinkModel = model.WithAssertAndCast<ListFieldLinkDefinition>("model", value => value.RequireNotNull());
+
+            var list = listModelHost.HostList;
+
+            DeployListFieldLink(modelHost, list, listFieldLinkModel);
+        }
+
+        private void DeployListFieldLink(object modelHost, List list, ListFieldLinkDefinition listFieldLinkModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
