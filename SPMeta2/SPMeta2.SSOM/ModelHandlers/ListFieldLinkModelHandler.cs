f66cb61f507a7be973032014395ab2fffce55d2b
SubPointSupport <support@subpointsolutions.com>
2014-08-24 14:32:06 -0700

    added ListFieldLinkDefinition and SSOM impl ([handlers] Cannot attach Site Columns to Lists, #77)

---
+++ SPMeta2/SPMeta2.SSOM/ModelHandlers/ListFieldLinkModelHandler.cs
@@ -0,0 +1,87 @@
+﻿using SPMeta2.Definitions;
+using SPMeta2.SSOM.ModelHosts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMeta2.Utils;
+using Microsoft.SharePoint;
+using SPMeta2.Common;
+
+namespace SPMeta2.SSOM.ModelHandlers
+{
+    public class ListFieldLinkModelHandler : SSOMModelHandlerBase
+    {
+        #region properties
+
+        public override Type TargetType
+        {
+            get { return typeof(ListFieldLinkDefinition); }
+        }
+
+        #endregion
+
+        #region methods
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var listModelHost = modelHost.WithAssertAndCast<ListModelHost>("modelHost", value => value.RequireNotNull());
+            var listFieldLinkModel = model.WithAssertAndCast<ListFieldLinkDefinition>("model", value => value.RequireNotNull());
+
+            var list = listModelHost.CurrentList;
+
+            DeployListFieldLink(modelHost, list, listFieldLinkModel);
+        }
+
+        private void DeployListFieldLink(object modelHost, SPList list, ListFieldLinkDefinition listFieldLinkModel)
+        {
+            var existingListField = list.Fields
+                                        .OfType<SPField>()
+                                        .FirstOrDefault(f => f.Id == listFieldLinkModel.FieldId);
+
+            InvokeOnModelEvent(this, new ModelEventArgs
+            {
+                CurrentModelNode = null,
+                Model = null,
+                EventType = ModelEventType.OnProvisioning,
+                Object = existingListField,
+                ObjectType = typeof(SPField),
+                ObjectDefinition = listFieldLinkModel,
+                ModelHost = modelHost
+            });
+
+            if (existingListField == null)
+            {
+                var siteField = list.ParentWeb.AvailableFields[listFieldLinkModel.FieldId];
+                list.Fields.Add(siteField);
+
+                InvokeOnModelEvent(this, new ModelEventArgs
+                {
+                    CurrentModelNode = null,
+                    Model = null,
+                    EventType = ModelEventType.OnProvisioned,
+                    Object = siteField,
+                    ObjectType = typeof(SPField),
+                    ObjectDefinition = listFieldLinkModel,
+                    ModelHost = modelHost
+                });
+            }
+            else
+            {
+                InvokeOnModelEvent(this, new ModelEventArgs
+                {
+                    CurrentModelNode = null,
+                    Model = null,
+                    EventType = ModelEventType.OnProvisioned,
+                    Object = existingListField,
+                    ObjectType = typeof(SPField),
+                    ObjectDefinition = listFieldLinkModel,
+                    ModelHost = modelHost
+                });
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ SPMeta2/SPMeta2/Definitions/ListFieldLinkDefinition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPMeta2.Definitions
+{
+    /// <summary>
+    ///  Allows to attach field to the target list.
+    /// </summary>
+    public class ListFieldLinkDefinition : DefinitionBase
+    {
+        #region properties
+
+        #region properties
+
+        /// <summary>
+        /// ID of the target field to be attached to content type.
+        /// BuiltInFieldId class can be used to utilize out of the box SharePoint fields. 
+        /// </summary>
+        public Guid FieldId { get; set; }
+
+        #endregion
+
+        #endregion
+    }
+}
