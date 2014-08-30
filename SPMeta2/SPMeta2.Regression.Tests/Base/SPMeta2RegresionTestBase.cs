a984365a11b1232ac12e05e31ce32a81b20235ed
SubPointSupport <support@subpointsolutions.com>
2014-08-29 18:17:51 -0700

    added SPMeta2.Regression.Environment.ps1, correct test runners initialization flow

--- SPMeta2/SPMeta2.Regression.Tests/Base/SPMeta2RegresionTestBase.cs
+++ SPMeta2/SPMeta2.Regression.Tests/Base/SPMeta2RegresionTestBase.cs
@@ -84,17 +84,23 @@ namespace SPMeta2.Regression.Tests.Base
 
             Trace.WriteLine(string.Format("Testing with runner libraries: [{0}]", runnerLibraries));
 
-            //if (!string.IsNullOrEmpty(runnerLibraries))
-            //{
-            //    var libs = runnerLibraries.Split(',');
+            if (!string.IsNullOrEmpty(runnerLibraries))
+            {
+                var libs = runnerLibraries.Split(',');
 
-            //    foreach (var lib in libs)
-            //        ProvisionRunnerAssemblies.Add(lib);
-            //}
+                foreach (var lib in libs)
+                    ProvisionRunnerAssemblies.Add(lib);
+            }
 
-            //            ProvisionRunnerAssemblies.Add("SPMeta2.Regression.Runners.O365.dll");
-            //ProvisionRunnerAssemblies.Add("SPMeta2.Regression.Runners.CSOM.dll");
-            ProvisionRunnerAssemblies.Add("SPMeta2.Regression.Runners.SSOM.dll");
+            if (ProvisionRunnerAssemblies.Count == 0)
+                throw new ArgumentException("Cannot find any test runners. Please configure test runners via SPMeta2.Regression.Environment.ps1 script.");
+
+            // Test runners should be managed via SPMeta2.Regression.Environment.ps1
+            // Manual adding is for internal use only.
+
+            //  ProvisionRunnerAssemblies.Add("SPMeta2.Regression.Runners.O365.dll");
+            //  ProvisionRunnerAssemblies.Add("SPMeta2.Regression.Runners.CSOM.dll");
+            //  ProvisionRunnerAssemblies.Add("SPMeta2.Regression.Runners.SSOM.dll");
         }
 
         protected ProvisionRunnerBase CurrentProvisionRunner;
