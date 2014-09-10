c41673271c50feadbe4e394a9f85531b3e77e8cd
SubPointSupport <support@subpointsolutions.com>
2014-09-10 05:58:24 -0700

    added WebScenariosTest

---
+++ SPMeta2/SPMeta2.Regression.Tests/Impl/Scenarios/WebScenariosTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.Definitions;
+using SPMeta2.Enumerations;
+using SPMeta2.Regression.Tests.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPMeta2.Regression.Tests.Impl.Scenarios
+{
+    [TestClass]
+    public class WebScenariosTest : SPMeta2RegresionEventsTestBase
+    {
+        #region internal
+
+        [ClassInitializeAttribute]
+        public static void Init(TestContext context)
+        {
+            InternalInit();
+        }
+
+        [ClassCleanupAttribute]
+        public static void Cleanup()
+        {
+            InternalCleanup();
+        }
+
+        #endregion
+
+        #region tests
+
+        [TestMethod]
+        [TestCategory("Regression.Scenarios.Webs")]
+        public void CanDeploy_BlankWeb()
+        {
+            TestRandomDefinition<WebDefinition>(def =>
+            {
+                def.WebTemplate = BuiltInWebTemplates.Collaboration.BlankSite;
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Regression.Scenarios.Webs")]
+        public void CanDeploy_BlogWeb()
+        {
+            TestRandomDefinition<WebDefinition>(def =>
+            {
+                def.WebTemplate = BuiltInWebTemplates.Collaboration.Blog;
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Regression.Scenarios.Webs")]
+        public void CanDeploy_CommunityWeb()
+        {
+            TestRandomDefinition<WebDefinition>(def =>
+            {
+                def.WebTemplate = BuiltInWebTemplates.Collaboration.CommunitySite;
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Regression.Scenarios.Webs")]
+        public void CanDeploy_TeamWeb()
+        {
+            TestRandomDefinition<WebDefinition>(def =>
+            {
+                def.WebTemplate = BuiltInWebTemplates.Collaboration.TeamSite;
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Regression.Scenarios.Webs")]
+        public void CanDeploy_WikiWeb()
+        {
+            TestRandomDefinition<WebDefinition>(def =>
+            {
+                def.WebTemplate = BuiltInWebTemplates.Collaboration.WikiSite;
+            });
+        }
+
+        #endregion
+    }
+}
