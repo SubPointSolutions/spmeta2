8a178b8684b6d916907b7c735bb13d50e4bac448
SubPointSupport <support@subpointsolutions.com>
2014-10-06 17:11:36 -0700

    added untracked files

---
+++ SPMeta2.Containers/RandomContainersSyntax.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using SPMeta2.Definitions;
+using SPMeta2.Models;
+using SPMeta2.Regression.Services;
+using SPMeta2.Syntax.Default.Extensions;
+
+namespace SPMeta2.Containers
+{
+    public static class RandomContainersSyntax
+    {
+        #region constructors
+
+        static RandomContainersSyntax()
+        {
+            ModelGeneratorService = new ModelGeneratorService();
+        }
+
+        #endregion
+
+        #region properties
+
+        public static ModelGeneratorService ModelGeneratorService { get; set; }
+
+        #endregion
+
+        #region syntax
+
+        #region webs
+
+        public static ModelNode AddRandomWeb(this ModelNode model)
+        {
+            return AddRandomWebPartPage(model, null);
+        }
+
+        public static ModelNode AddRandomWeb(this ModelNode model, Action<ModelNode> action)
+        {
+            return model.AddRandomDefinition<WebDefinition>(action);
+        }
+
+        #endregion
+
+        #region webpart pages
+
+        public static ModelNode AddRandomWebPartPage(this ModelNode model)
+        {
+            return AddRandomWebPartPage(model, null);
+        }
+
+        public static ModelNode AddRandomWebPartPage(this ModelNode model, Action<ModelNode> action)
+        {
+            return model.AddRandomDefinition<WebPartPageDefinition>(action);
+        }
+
+        #endregion
+
+        #region publishing pages
+
+        public static ModelNode AddRandomPublishingPage(this ModelNode model)
+        {
+            return AddRandomWebPartPage(model, null);
+        }
+
+        public static ModelNode AddRandomPublishingPage(this ModelNode model, Action<ModelNode> action)
+        {
+            return model.AddRandomDefinition<PublishingPageDefinition>(action);
+        }
+
+        #endregion
+
+        #region lists
+
+        public static ModelNode AddRandomList(this ModelNode model)
+        {
+            return AddRandomWebPartPage(model, null);
+        }
+
+        public static ModelNode AddRandomList(this ModelNode model, Action<ModelNode> action)
+        {
+            return model.AddRandomDefinition<ListDefinition>(action);
+        }
+
+        #endregion
+
+        #region web parts
+
+        public static ModelNode AddRandomWebpart(this ModelNode model)
+        {
+            return AddRandomWebpart(model, null);
+        }
+
+        public static ModelNode AddRandomWebpart(this ModelNode model, Action<ModelNode> action)
+        {
+            return model.AddRandomDefinition<WebPartDefinition>(action);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region internal
+
+        public static ModelNode AddRandomDefinition<TDefinition>(this ModelNode model)
+            where TDefinition : DefinitionBase
+        {
+            return AddRandomDefinition<TDefinition>(model, null);
+        }
+
+        public static ModelNode AddRandomDefinition<TDefinition>(this ModelNode model, Action<ModelNode> action)
+              where TDefinition : DefinitionBase
+        {
+            return model.AddDefinitionNode(ModelGeneratorService.GetRandomDefinition<TDefinition>(), action);
+        }
+
+        #endregion
+    }
+}
---
+++ SPMeta2/SPMeta2/Definitions/Base/SolutionDefinitionBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMeta2.Attributes.Regression;
+
+namespace SPMeta2.Definitions.Base
+{
+    public abstract class SolutionDefinitionBase : DefinitionBase
+    {
+       
+
+        #region properties
+
+        /// <summary>
+        /// Target solutions file name.
+        /// </summary>
+        /// 
+        [ExpectValidation]
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Target sandbox solution content.
+        /// </summary>
+        [ExpectValidation]
+        public byte[] Content { get; set; }
+
+
+
+        #endregion
+    }
+}
