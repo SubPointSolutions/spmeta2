95aa2a7cfedc89d956e4df86282aa147c6638e13
SubPointSupport <support@subpointsolutions.com>
2014-11-10 15:30:55 -0800

    added BuiltInBasePermissions class

---
+++ SPMeta2/SPMeta2/Enumerations/BuiltInBasePermissions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPMeta2.Enumerations
+{
+    public static class BuiltInBasePermissions
+    {
+        #region properties
+
+        public static string EmptyMask = "EmptyMask";
+        public static string ViewListItems = "ViewListItems";
+        public static string AddListItems = "AddListItems";
+        public static string EditListItems = "EditListItems";
+        public static string DeleteListItems = "DeleteListItems";
+        public static string ApproveItems = "ApproveItems";
+        public static string OpenItems = "OpenItems";
+        public static string ViewVersions = "ViewVersions";
+        public static string DeleteVersions = "DeleteVersions";
+        public static string CancelCheckout = "CancelCheckout";
+        public static string ManagePersonalViews = "ManagePersonalViews";
+        public static string ManageLists = "ManageLists";
+        public static string ViewFormPages = "ViewFormPages";
+        public static string AnonymousSearchAccessList = "AnonymousSearchAccessList";
+        public static string Open = "Open";
+        public static string ViewPages = "ViewPages";
+        public static string AddAndCustomizePages = "AddAndCustomizePages";
+        public static string ApplyThemeAndBorder = "ApplyThemeAndBorder";
+        public static string ApplyStyleSheets = "ApplyStyleSheets";
+        public static string ViewUsageData = "ViewUsageData";
+        public static string CreateSSCSite = "CreateSSCSite";
+        public static string ManageSubwebs = "ManageSubwebs";
+        public static string CreateGroups = "CreateGroups";
+        public static string ManagePermissions = "ManagePermissions";
+        public static string BrowseDirectories = "BrowseDirectories";
+        public static string BrowseUserInfo = "BrowseUserInfo";
+        public static string AddDelPrivateWebParts = "AddDelPrivateWebParts";
+        public static string UpdatePersonalWebParts = "UpdatePersonalWebParts";
+        public static string ManageWeb = "ManageWeb";
+        public static string AnonymousSearchAccessWebLists = "AnonymousSearchAccessWebLists";
+        public static string UseClientIntegration = "UseClientIntegration";
+        public static string UseRemoteAPIs = "UseRemoteAPIs";
+        public static string ManageAlerts = "ManageAlerts";
+        public static string CreateAlerts = "CreateAlerts";
+        public static string EditMyUserInfo = "EditMyUserInfo";
+        public static string EnumeratePermissions = "EnumeratePermissions";
+        public static string FullMask = "FullMask";
+
+        #endregion
+    }
+}
