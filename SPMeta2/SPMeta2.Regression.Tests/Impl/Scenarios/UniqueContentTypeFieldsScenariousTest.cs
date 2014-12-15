bb7cea0aa1efe18ed56735e790780704e5ab0adc
SubPointSupport <support@subpointsolutions.com>
2014-12-15 05:47:42 -0800

    failed CanDeploy_UniqueContentTypeFieldsOrder_ForIssueContentType test, #254

---
+++ SPMeta2/SPMeta2.Regression.Tests/Impl/Scenarios/UniqueContentTypeFieldsScenariousTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.Containers;
+using SPMeta2.Definitions;
+using SPMeta2.Definitions.ContentTypes;
+using SPMeta2.Enumerations;
+using SPMeta2.Regression.Tests.Impl.Scenarios.Base;
+using SPMeta2.Syntax.Default;
+
+namespace SPMeta2.Regression.Tests.Impl.Scenarios
+{
+    [TestClass]
+    public class UniqueContentTypeFieldsScenariousTest : SPMeta2RegresionScenarioTestBase
+    {
+        #region internal
+
+        [ClassInitializeAttribute]
+        public static void Init(TestContext context)
+        {
+            InternalInit();
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            InternalCleanup();
+        }
+
+        #endregion
+
+
+        #region tests
+
+        [TestMethod]
+        [TestCategory("Regression.Scenarios.UniqueContentTypeFields")]
+        public void CanDeploy_UniqueContentTypeFieldsOrder_ForIssueContentType()
+        {
+            var ctDef = ModelGeneratorService.GetRandomDefinition<ContentTypeDefinition>(def =>
+            {
+                def.ParentContentTypeId = BuiltInContentTypeId.Issue;
+            });
+
+            var model = SPMeta2Model
+                .NewSiteModel(site =>
+                {
+                    site.AddContentType(ctDef, contentType =>
+                    {
+                        contentType
+                            .AddUniqueContentTypeFieldsOrder(new UniqueContentTypeFieldsOrderDefinition
+                            {
+                                Fields = new List<FieldLinkValue>
+                                {
+                                    new FieldLinkValue{ InternalName = BuiltInInternalFieldNames.TaskDueDate },
+                                    new FieldLinkValue{ InternalName = BuiltInInternalFieldNames.Title }
+                                }
+                            });
+
+                    });
+                });
+
+            TestModel(model);
+        }
+
+        #endregion
+    }
+}
