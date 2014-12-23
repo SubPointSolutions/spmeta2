889e47ea7fd159d0b747a185e08b28915a70b2ca
SubPointSupport <support@subpointsolutions.com>
2014-12-23 01:39:55 -0800

    Passed CanDeployRandom_URLFieldDefinition() test for CSOM.

---
+++ SPMeta2/SPMeta2.Regression.CSOM/Validation/Fields/URLFieldDefinitionValidator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+using SPMeta2.Definitions;
+using SPMeta2.Definitions.Fields;
+using SPMeta2.Utils;
+
+namespace SPMeta2.Regression.CSOM.Validation.Fields
+{
+    public class URLFieldDefinitionValidator : ClientFieldDefinitionValidator
+    {
+        public override Type TargetType
+        {
+            get
+            {
+                return typeof(URLFieldDefinition);
+            }
+        }
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var definition = model.WithAssertAndCast<FieldDefinition>("model", value => value.RequireNotNull());
+            var spObject = GetField(modelHost, definition);
+
+            var assert = ServiceFactory.AssertService.NewAssert(model, definition, spObject);
+
+            ValidateField(assert, spObject, definition);
+
+            var typedField = spObject.Context.CastTo<FieldUrl>(spObject);
+            var typedDefinition = model.WithAssertAndCast<URLFieldDefinition>("model", value => value.RequireNotNull());
+
+            var typedFieldAssert = ServiceFactory.AssertService.NewAssert(model, typedDefinition, typedField);
+
+            typedFieldAssert.ShouldBeEqual(m => m.DisplayFormat, o => o.GetDisplayFormat());
+
+        }
+    }
+
+    internal static class SPFieldUrlExtensions
+    {
+        public static string GetDisplayFormat(this FieldUrl field)
+        {
+            return field.DisplayFormat.ToString();
+        }
+    }
+}
