9a39dec02bb3db207834197131bd21ae80b0568f
SubPointSupport <support@subpointsolutions.com>
2014-12-23 03:12:32 -0800

    Passed CanDeployRandom_InformationRightsManagementSettingsDefinition() test for CSOM.

---
+++ SPMeta2/SPMeta2.CSOM/ModelHandlers/InformationRightsManagementSettingsModelHandler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+using SPMeta2.Common;
+using SPMeta2.CSOM.ModelHosts;
+using SPMeta2.Definitions;
+using SPMeta2.Utils;
+
+namespace SPMeta2.CSOM.ModelHandlers
+{
+    public class InformationRightsManagementSettingsModelHandler : CSOMModelHandlerBase
+    {
+        #region properties
+
+        public override Type TargetType
+        {
+            get { return typeof(InformationRightsManagementSettingsDefinition); }
+        }
+
+        #endregion
+
+        #region methods
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var listModelHost = modelHost.WithAssertAndCast<ListModelHost>("modelHost", value => value.RequireNotNull());
+            var definition = model.WithAssertAndCast<InformationRightsManagementSettingsDefinition>("model", value => value.RequireNotNull());
+
+            DeployDefinition(modelHost, listModelHost, definition);
+        }
+
+        private void DeployDefinition(object modelHost, ListModelHost siteModelHost, InformationRightsManagementSettingsDefinition definition)
+        {
+            var currentSettings = GetCurrentInformationRightsManagementSettings(siteModelHost.HostList);
+
+            InvokeOnModelEvent(this, new ModelEventArgs
+            {
+                CurrentModelNode = null,
+                Model = null,
+                EventType = ModelEventType.OnProvisioning,
+                Object = currentSettings,
+                ObjectType = typeof(InformationRightsManagementSettings),
+                ObjectDefinition = definition,
+                ModelHost = modelHost
+            });
+
+            MapInformationRightsManagementSettings(currentSettings, definition);
+
+            InvokeOnModelEvent(this, new ModelEventArgs
+            {
+                CurrentModelNode = null,
+                Model = null,
+                EventType = ModelEventType.OnProvisioned,
+                Object = currentSettings,
+                ObjectType = typeof(InformationRightsManagementSettings),
+                ObjectDefinition = definition,
+                ModelHost = modelHost
+            });
+
+            currentSettings.Update();
+        }
+
+        private void MapInformationRightsManagementSettings(InformationRightsManagementSettings currentSettings, InformationRightsManagementSettingsDefinition definition)
+        {
+            currentSettings.AllowPrint = definition.AllowPrint;
+            currentSettings.AllowScript = definition.AllowScript;
+            currentSettings.AllowWriteCopy = definition.AllowWriteCopy;
+
+            currentSettings.DisableDocumentBrowserView = definition.DisableDocumentBrowserView;
+            currentSettings.DocumentAccessExpireDays = definition.DocumentAccessExpireDays;
+            currentSettings.DocumentLibraryProtectionExpireDate = definition.DocumentLibraryProtectionExpireDate;
+
+            currentSettings.EnableDocumentAccessExpire = definition.EnableDocumentAccessExpire;
+            currentSettings.EnableDocumentBrowserPublishingView = definition.EnableDocumentBrowserPublishingView;
+            currentSettings.EnableGroupProtection = definition.EnableGroupProtection;
+            currentSettings.EnableLicenseCacheExpire = definition.EnableLicenseCacheExpire;
+
+            currentSettings.GroupName = definition.GroupName;
+            currentSettings.LicenseCacheExpireDays = definition.LicenseCacheExpireDays;
+
+            currentSettings.PolicyDescription = definition.PolicyDescription;
+            currentSettings.PolicyTitle = definition.PolicyTitle;
+        }
+
+        protected InformationRightsManagementSettings GetCurrentInformationRightsManagementSettings(List list)
+        {
+            return list.InformationRightsManagementSettings;
+        }
+
+        #endregion
+    }
+}
---
+++ SPMeta2/SPMeta2.Regression.CSOM/Validation/ClientInformationRightsManagementSettingsDefinitionValidator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using SPMeta2.Containers.Assertion;
+using SPMeta2.CSOM.ModelHandlers;
+using SPMeta2.CSOM.ModelHosts;
+using SPMeta2.Definitions;
+using SPMeta2.Utils;
+
+namespace SPMeta2.Regression.CSOM.Validation
+{
+    public class ClientInformationRightsManagementSettingsDefinitionValidator : InformationRightsManagementSettingsModelHandler
+    {
+        #region methods
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var siteModelHost = modelHost.WithAssertAndCast<ListModelHost>("modelHost", value => value.RequireNotNull());
+
+            var definition = model.WithAssertAndCast<InformationRightsManagementSettingsDefinition>("model", value => value.RequireNotNull());
+            var spObject = GetCurrentInformationRightsManagementSettings(siteModelHost.HostList);
+
+            var assert = ServiceFactory.AssertService
+                  .NewAssert(definition, definition, spObject)
+                  .ShouldNotBeNull(spObject)
+                  .ShouldBeEqual(m => m.AllowPrint, o => o.AllowPrint)
+                  .ShouldBeEqual(m => m.AllowScript, o => o.AllowScript)
+                  .ShouldBeEqual(m => m.AllowWriteCopy, o => o.AllowWriteCopy)
+                  .ShouldBeEqual(m => m.DisableDocumentBrowserView, o => o.DisableDocumentBrowserView)
+                //.ShouldBeEqual(m => m.DocumentLibraryProtectionExpireDate, o => o.DocumentLibraryProtectionExpireDate)
+                  .ShouldBeEqual(m => m.EnableDocumentAccessExpire, o => o.EnableDocumentAccessExpire)
+                  .ShouldBeEqual(m => m.EnableDocumentBrowserPublishingView, o => o.EnableDocumentBrowserPublishingView)
+                  .ShouldBeEqual(m => m.EnableGroupProtection, o => o.EnableGroupProtection)
+                  .ShouldBeEqual(m => m.GroupName, o => o.GroupName)
+                //.ShouldBeEqual(m => m.LicenseCacheExpireDays, o => o.LicenseCacheExpireDays)
+                  .ShouldBeEqual(m => m.PolicyTitle, o => o.PolicyTitle)
+                  .ShouldBeEqual(m => m.PolicyDescription, o => o.PolicyDescription)
+                  .ShouldBeEqual(m => m.EnableLicenseCacheExpire, o => o.EnableLicenseCacheExpire)
+                  .ShouldBeEqual(m => m.DocumentAccessExpireDays, o => o.DocumentAccessExpireDays);
+
+            assert.ShouldBeEqual((p, s, d) =>
+            {
+                var srcProp = s.GetExpressionValue(def => def.DocumentLibraryProtectionExpireDate);
+                var dstProp = d.GetExpressionValue(ct => ct.DocumentLibraryProtectionExpireDate);
+
+                var isValid = ((DateTime)srcProp.Value).ToShortDateString() == ((DateTime)dstProp.Value).ToShortDateString();
+
+                return new PropertyValidationResult
+                  {
+                      Tag = p.Tag,
+                      Src = srcProp,
+                      Dst = dstProp,
+                      IsValid = isValid
+                  };
+            });
+
+            // // SSOM hardcoded value, reflection
+            if (definition.EnableLicenseCacheExpire)
+            {
+                assert.ShouldBeEqual(m => m.LicenseCacheExpireDays, o => o.LicenseCacheExpireDays);
+            }
+            else
+            {
+                assert.ShouldBeEqual((p, s, d) =>
+                {
+                    var srcProp = s.GetExpressionValue(def => def.LicenseCacheExpireDays);
+                    var dstProp = d.GetExpressionValue(ct => ct.LicenseCacheExpireDays);
+
+                    var isValid =
+
+                        // SSOM hardcoded value, reflection
+                        d.LicenseCacheExpireDays == 30 ||
+                        // CSOM regular one
+                        s.LicenseCacheExpireDays == d.LicenseCacheExpireDays;
+
+                    return new PropertyValidationResult
+                    {
+                        Tag = p.Tag,
+                        Src = srcProp,
+                        Dst = dstProp,
+                        IsValid = isValid
+                    };
+                });
+            }
+        }
+
+        #endregion
+    }
+}
