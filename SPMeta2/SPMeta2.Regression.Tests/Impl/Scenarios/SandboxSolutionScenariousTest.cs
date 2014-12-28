2d23c3f126f62b3255775fcf57d569d5e008e9ff
SubPointSupport <support@subpointsolutions.com>
2014-12-27 23:18:59 -0800

    Second sandbox solution provision gives exception #273 - CSOM limitation: DesignPackage.UnInstall() cannot deactivate and uninstall package w/ dots in the name.

---
+++ SPMeta2/SPMeta2.Regression.Tests/Impl/Scenarios/SandboxSolutionScenariousTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.Containers;
+using SPMeta2.Containers.Services;
+using SPMeta2.Definitions;
+using SPMeta2.Regression.Tests.Base;
+using SPMeta2.Regression.Tests.Impl.Scenarios.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMeta2.Syntax.Default;
+using System.IO;
+
+namespace SPMeta2.Regression.Tests.Impl.Scenarios
+{
+    [TestClass]
+    public class SandboxSolutionScenariousTest : SPMeta2RegresionScenarioTestBase
+    {
+        #region internal
+
+        [ClassInitializeAttribute]
+        public static void Init(TestContext context)
+        {
+            InternalInit();
+        }
+
+        [ClassCleanupAttribute]
+        public static void Cleanup()
+        {
+            InternalCleanup();
+        }
+
+        #endregion
+
+        #region default
+
+       
+        #endregion
+    }
+}
