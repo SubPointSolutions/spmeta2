c75093e93d0656dfb2489d646ce19d6633c3ba23
SubPointSupport <support@subpointsolutions.com>
2015-01-05 05:17:44 -0800

    CanDeployRandom_TreeViewSettingsDefinition test passed for CSOM, Add 'Tree View Settings' provision support #278

---
+++ SPMeta2/SPMeta2.CSOM/ModelHandlers/TreeViewSettingsModelHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+using SPMeta2.Common;
+using SPMeta2.CSOM.ModelHosts;
+using SPMeta2.Definitions;
+using SPMeta2.Utils;
+
+namespace SPMeta2.CSOM.ModelHandlers
+{
+    public class TreeViewSettingsModelHandler : CSOMModelHandlerBase
+    {
+        #region methods
+
+        public override Type TargetType
+        {
+            get { return typeof(TreeViewSettingsDefinition); }
+        }
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var webModelHost = modelHost.WithAssertAndCast<WebModelHost>("modelHost", value => value.RequireNotNull());
+            var definition = model.WithAssertAndCast<TreeViewSettingsDefinition>("model", value => value.RequireNotNull());
+
+            var web = webModelHost.HostWeb;
+            var context = web.Context;
+
+            InvokeOnModelEvent(this, new ModelEventArgs
+            {
+                CurrentModelNode = null,
+                Model = null,
+                EventType = ModelEventType.OnProvisioning,
+                Object = web,
+                ObjectType = typeof(Web),
+                ObjectDefinition = definition,
+                ModelHost = modelHost
+            });
+
+            if (definition.QuickLaunchEnabled.HasValue)
+                web.QuickLaunchEnabled = definition.QuickLaunchEnabled.Value;
+
+            if (definition.TreeViewEnabled.HasValue)
+                web.TreeViewEnabled = definition.TreeViewEnabled.Value;
+
+            InvokeOnModelEvent(this, new ModelEventArgs
+            {
+                CurrentModelNode = null,
+                Model = null,
+                EventType = ModelEventType.OnProvisioned,
+                Object = web,
+                ObjectType = typeof(Web),
+                ObjectDefinition = definition,
+                ModelHost = modelHost
+            });
+
+            if (definition.QuickLaunchEnabled.HasValue || definition.TreeViewEnabled.HasValue)
+            {
+                web.Update();
+                context.ExecuteQuery();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ SPMeta2/SPMeta2.Regression.CSOM/Validation/ClientTreeViewSettingsDefinitionValidator.cs
@@ -0,0 +1,36 @@
+﻿using SPMeta2.CSOM.ModelHandlers;
+using SPMeta2.CSOM.ModelHosts;
+using SPMeta2.Definitions;
+using SPMeta2.Utils;
+
+namespace SPMeta2.Regression.CSOM.Validation
+{
+    public class ClientTreeViewSettingsDefinitionValidator : TreeViewSettingsModelHandler
+    {
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var webModelHost = modelHost.WithAssertAndCast<WebModelHost>("modelHost", value => value.RequireNotNull());
+            var definition = model.WithAssertAndCast<TreeViewSettingsDefinition>("model", value => value.RequireNotNull());
+
+            var spObject = webModelHost.HostWeb;
+            var context = spObject.Context;
+
+            context.Load(spObject);
+            context.ExecuteQuery();
+
+            var assert = ServiceFactory.AssertService
+                               .NewAssert(definition, spObject)
+                               .ShouldNotBeNull(spObject);
+
+            if (definition.QuickLaunchEnabled.HasValue)
+                assert.ShouldBeEqual(m => m.QuickLaunchEnabled, o => o.QuickLaunchEnabled);
+            else
+                assert.SkipProperty(m => m.QuickLaunchEnabled, "QuickLaunchEnabled is NULL. Skipping.");
+
+            if (definition.TreeViewEnabled.HasValue)
+                assert.ShouldBeEqual(m => m.TreeViewEnabled, o => o.QuickLaunchEnabled);
+            else
+                assert.SkipProperty(m => m.TreeViewEnabled, "QuickLaunchEnabled is NULL. Skipping.");
+        }
+    }
+}
