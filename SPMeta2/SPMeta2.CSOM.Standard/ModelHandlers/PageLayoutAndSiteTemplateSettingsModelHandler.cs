3e36ac6d4cef2f756b1d197370f82769075a4384
SubPointSupport <support@subpointsolutions.com>
2015-01-06 12:23:20 -0800

    PageLayoutAndSiteTemplateSettingsDefinition, CSOM bootstrapping.

---
+++ SPMeta2/SPMeta2.CSOM.Standard/ModelHandlers/PageLayoutAndSiteTemplateSettingsModelHandler.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.Publishing;
+using SPMeta2.Common;
+using SPMeta2.CSOM.ModelHosts;
+using SPMeta2.Definitions;
+using SPMeta2.Standard.Definitions;
+using SPMeta2.Utils;
+using SPMeta2.CSOM.ModelHandlers;
+
+namespace SPMeta2.CSOM.Standard.ModelHandlers
+{
+    public class PageLayoutAndSiteTemplateSettingsModelHandler : CSOMModelHandlerBase
+    {
+        #region properties
+
+        public override Type TargetType
+        {
+            get { return typeof(PageLayoutAndSiteTemplateSettingsDefinition); }
+        }
+
+        #endregion
+
+        #region methods
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var webModelHost = modelHost.WithAssertAndCast<WebModelHost>("modelHost", value => value.RequireNotNull());
+            var definition = model.WithAssertAndCast<PageLayoutAndSiteTemplateSettingsDefinition>("model", value => value.RequireNotNull());
+
+            DeployDefinition(modelHost, webModelHost, definition);
+        }
+
+
+        private void DeployDefinition(object modelHost, WebModelHost webModelHost, PageLayoutAndSiteTemplateSettingsDefinition definition)
+        {
+            var web = webModelHost.HostWeb;
+            var context = web.Context;
+
+            var publishingWeb = PublishingWeb.GetPublishingWeb(context, web);
+
+            InvokeOnModelEvent(this, new ModelEventArgs
+            {
+                CurrentModelNode = null,
+                Model = null,
+                EventType = ModelEventType.OnProvisioning,
+                Object = web,
+                ObjectType = typeof(Web),
+                ObjectDefinition = definition,
+                ModelHost = modelHost
+            });
+
+            ProcessWebTemplateSettings(publishingWeb, definition);
+            ProcessPageLayoutSettings(publishingWeb, definition);
+            ProcessNewPageDefaultSettings(publishingWeb, definition);
+            ProcessConverBlankSpacesIntoHyphenSetting(publishingWeb, definition);
+
+            InvokeOnModelEvent(this, new ModelEventArgs
+            {
+                CurrentModelNode = null,
+                Model = null,
+                EventType = ModelEventType.OnProvisioned,
+                Object = web,
+                ObjectType = typeof(Web),
+                ObjectDefinition = definition,
+                ModelHost = modelHost
+            });
+
+            web.Update();
+            context.ExecuteQuery();
+        }
+
+        private void ProcessConverBlankSpacesIntoHyphenSetting(PublishingWeb publishingWeb, PageLayoutAndSiteTemplateSettingsDefinition definition)
+        {
+            var web = publishingWeb.Web;
+            var context = web.Context;
+
+            var key = "__AllowSpacesInNewPageName";
+            var value = definition.ConverBlankSpacesIntoHyphen.HasValue
+                ? definition.ConverBlankSpacesIntoHyphen.ToString()
+                : false.ToString();
+
+            context.Load(web, w => w.AllProperties);
+
+            if (!web.AllProperties.FieldValues.ContainsKey(key))
+                web.AllProperties.FieldValues.Add(key, value);
+            else
+                web.AllProperties.FieldValues[key] = value;
+
+            context.ExecuteQuery();
+        }
+
+        private static void ProcessNewPageDefaultSettings(PublishingWeb publishingWeb, PageLayoutAndSiteTemplateSettingsDefinition definition)
+        {
+            //var web = publishingWeb.Web;
+
+            //if (definition.InheritDefaultPageLayout.HasValue && definition.InheritDefaultPageLayout.Value)
+            //    publishingWeb.InheritDefaultPageLayout();
+            //else if (definition.UseDefinedDefaultPageLayout.HasValue && definition.UseDefinedDefaultPageLayout.Value)
+            //{
+            //    var publishingSite = new PublishingSite(web.Site);
+            //    var pageLayouts = publishingSite.PageLayouts;
+
+            //    var selectedPageLayout = pageLayouts.FirstOrDefault(t => t.Name.ToUpper() == definition.DefinedDefaultPageLayout.ToUpper()); ;
+
+            //    if (selectedPageLayout != null)
+            //    {
+            //        publishingWeb.SetDefaultPageLayout(
+            //            selectedPageLayout,
+            //            definition.ResetAllSubsitesToInheritDefaultPageLayout.HasValue
+            //                ? definition.ResetAllSubsitesToInheritDefaultPageLayout.Value
+            //                : false);
+            //    }
+            //}
+        }
+
+        private static void ProcessPageLayoutSettings(PublishingWeb publishingWeb, PageLayoutAndSiteTemplateSettingsDefinition definition)
+        {
+            //var web = publishingWeb.Web;
+
+            //if (definition.InheritPageLayouts.HasValue && definition.InheritPageLayouts.Value)
+            //    publishingWeb.InheritAvailablePageLayouts();
+            //else if (definition.UseAnyPageLayout.HasValue && definition.UseAnyPageLayout.Value)
+            //{
+            //    publishingWeb.AllowAllPageLayouts(definition.ResetAllSubsitesToInheritPageLayouts.HasValue
+            //        ? definition.ResetAllSubsitesToInheritPageLayouts.Value
+            //        : false);
+            //}
+            //else if (definition.UseDefinedPageLayouts.HasValue && definition.UseDefinedPageLayouts.Value)
+            //{
+            //    var publishingSite = new PublishingSite(web.Site);
+            //    var pageLayouts = publishingSite.PageLayouts;
+
+            //    var selectedPageLayouts = new List<PageLayout>();
+
+            //    foreach (var selectedLayoutName in definition.DefinedPageLayouts)
+            //    {
+            //        var targetLayout = pageLayouts.FirstOrDefault(t => t.Name.ToUpper() == selectedLayoutName.ToUpper());
+
+            //        if (targetLayout != null)
+            //            selectedPageLayouts.Add(targetLayout);
+            //    }
+
+            //    if (selectedPageLayouts.Any())
+            //    {
+            //        publishingWeb.SetAvailablePageLayouts(selectedPageLayouts.ToArray(),
+            //            definition.ResetAllSubsitesToInheritPageLayouts.HasValue
+            //                ? definition.ResetAllSubsitesToInheritPageLayouts.Value
+            //                : false);
+            //    }
+            //}
+        }
+
+        private static void ProcessWebTemplateSettings(PublishingWeb publishingWeb, PageLayoutAndSiteTemplateSettingsDefinition definition)
+        {
+            //var web = publishingWeb.Web;
+
+            //if (definition.InheritWebTemplates.HasValue && definition.InheritWebTemplates.Value)
+            //    publishingWeb.InheritAvailableWebTemplates();
+            //else if (definition.UseAnyWebTemplate.HasValue && definition.UseAnyWebTemplate.Value)
+            //{
+            //    publishingWeb.AllowAllWebTemplates(definition.ResetAllSubsitesToInheritWebTemplates.HasValue
+            //        ? definition.ResetAllSubsitesToInheritWebTemplates.Value
+            //        : false);
+            //}
+            //else if (definition.UseDefinedWebTemplates.HasValue && definition.UseDefinedWebTemplates.Value)
+            //{
+            //    var currentLocaleId = (uint)web.CurrencyLocaleID;
+            //    var webTemplates = new List<SPWebTemplate>();
+
+            //    webTemplates.AddRange(web.Site.GetWebTemplates(currentLocaleId).OfType<SPWebTemplate>());
+            //    webTemplates.AddRange(web.Site.GetCustomWebTemplates(currentLocaleId).OfType<SPWebTemplate>());
+
+            //    var selectedWebTemplates = new Collection<SPWebTemplate>();
+
+            //    foreach (var selectedWebTemplateName in definition.DefinedWebTemplates)
+            //    {
+            //        var targetWebTemplate =
+            //            webTemplates.FirstOrDefault(t => t.Name.ToUpper() == selectedWebTemplateName.ToUpper());
+
+            //        if (targetWebTemplate != null)
+            //            selectedWebTemplates.Add(targetWebTemplate);
+            //    }
+
+            //    if (selectedWebTemplates.Any())
+            //    {
+            //        publishingWeb.SetAvailableWebTemplates(selectedWebTemplates, 0,
+            //            definition.ResetAllSubsitesToInheritWebTemplates.HasValue
+            //                ? definition.ResetAllSubsitesToInheritWebTemplates.Value
+            //                : false);
+            //    }
+            //}
+        }
+
+        #endregion
+    }
+}
---
+++ SPMeta2/SPMeta2.Regression.CSOM.Standard/Validation/ClientPageLayoutAndSiteTemplateSettingsDefinitionValidator.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using Microsoft.SharePoint.Client.Publishing;
+using SPMeta2.Containers.Assertion;
+using SPMeta2.CSOM.ModelHosts;
+using SPMeta2.CSOM.Standard.ModelHandlers;
+using SPMeta2.Definitions;
+using SPMeta2.Standard.Definitions;
+using SPMeta2.Utils;
+
+namespace SPMeta2.Regression.CSOM.Standard.Validation
+{
+    public class ClientPageLayoutAndSiteTemplateSettingsDefinitionValidator : PageLayoutAndSiteTemplateSettingsModelHandler
+    {
+        #region methods
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var webModelHost = modelHost.WithAssertAndCast<WebModelHost>("modelHost", value => value.RequireNotNull());
+            var definition = model.WithAssertAndCast<PageLayoutAndSiteTemplateSettingsDefinition>("model", value => value.RequireNotNull());
+
+            var context = webModelHost.HostWeb.Context;
+            var spObject = PublishingWeb.GetPublishingWeb(context, webModelHost.HostWeb);
+
+            var assert = ServiceFactory.AssertService
+                            .NewAssert(definition, spObject)
+                            .ShouldNotBeNull(spObject);
+
+            //// web templates
+            //if (definition.InheritWebTemplates.HasValue)
+            //    assert.ShouldBeEqual(m => m.InheritWebTemplates, o => o.IsInheritingAvailableWebTemplates);
+            //else
+            //    assert.SkipProperty(m => m.InheritWebTemplates, "InheritWebTemplates is NULL. Skipping");
+
+            //if (definition.UseAnyWebTemplate.HasValue)
+            //    assert.ShouldBeEqual(m => m.UseAnyWebTemplate, o => o.IsAllowingAllWebTemplates);
+            //else
+            //    assert.SkipProperty(m => m.UseAnyWebTemplate, "UseAnyWebTemplate is NULL. Skipping");
+
+            //// TODO
+            //if (definition.UseDefinedWebTemplates.HasValue)
+            //{
+            //    assert.SkipProperty(m => m.UseDefinedWebTemplates, "UseDefinedWebTemplates is not NULL. Validating DefinedWebTemplates.");
+
+            //    assert.ShouldBeEqual((p, s, d) =>
+            //    {
+            //        var srcProp = s.GetExpressionValue(m => m.DefinedWebTemplates);
+            //        var isValid = true;
+
+            //        var currentTemplates = spObject.GetAvailableWebTemplates((uint)spObject.Web.CurrencyLocaleID);
+            //        var definedTemplates = definition.DefinedWebTemplates;
+
+            //        foreach (var defTemplate in definedTemplates)
+            //        {
+            //            if (!currentTemplates.OfType<SPWebTemplate>().Any(t => t.Name.ToUpper() == defTemplate.ToUpper()))
+            //                isValid = false;
+            //        }
+
+            //        return new PropertyValidationResult
+            //        {
+            //            Tag = p.Tag,
+            //            Src = srcProp,
+            //            Dst = null,
+            //            IsValid = isValid
+            //        };
+            //    });
+
+            //}
+            //else
+            //{
+            //    assert.SkipProperty(m => m.UseDefinedWebTemplates, "UseDefinedWebTemplates is NULL. Skipping");
+            //    assert.SkipProperty(m => m.DefinedWebTemplates, "DefinedWebTemplates as UseDefinedWebTemplates is NULL. Skipping");
+            //}
+
+            //assert.SkipProperty(m => m.ResetAllSubsitesToInheritWebTemplates, "ResetAllSubsitesToInheritWebTemplates can't be validates. Skipping.");
+
+            //// layouts
+            //if (definition.InheritPageLayouts.HasValue)
+            //    assert.ShouldBeEqual(m => m.InheritPageLayouts, o => o.IsInheritingAvailablePageLayouts);
+            //else
+            //    assert.SkipProperty(m => m.InheritPageLayouts, "InheritPageLayouts is NULL. Skipping");
+
+            //if (definition.UseAnyPageLayout.HasValue)
+            //    assert.ShouldBeEqual(m => m.UseAnyPageLayout, o => o.IsAllowingAllPageLayouts);
+            //else
+            //    assert.SkipProperty(m => m.UseAnyPageLayout, "UseAnyPageLayout is NULL. Skipping");
+
+            //assert.SkipProperty(m => m.ResetAllSubsitesToInheritPageLayouts, "ResetAllSubsitesToInheritPageLayouts can't be validates. Skipping.");
+
+            //if (definition.UseDefinedPageLayouts.HasValue)
+            //{
+            //    assert.SkipProperty(m => m.UseDefinedPageLayouts, "UseDefinedPageLayouts is not NULL. Validating DefinedPageLayouts.");
+
+            //    assert.ShouldBeEqual((p, s, d) =>
+            //    {
+            //        var srcProp = s.GetExpressionValue(m => m.DefinedPageLayouts);
+            //        var isValid = true;
+
+            //        var currentTemplates = spObject.GetAvailablePageLayouts();
+            //        var definedTemplates = definition.DefinedPageLayouts;
+
+            //        foreach (var defTemplate in definedTemplates)
+            //        {
+            //            if (!currentTemplates.OfType<PageLayout>().Any(t => t.Name.ToUpper() == defTemplate.ToUpper()))
+            //                isValid = false;
+            //        }
+
+            //        return new PropertyValidationResult
+            //        {
+            //            Tag = p.Tag,
+            //            Src = srcProp,
+            //            Dst = null,
+            //            IsValid = isValid
+            //        };
+            //    });
+
+            //}
+            //else
+            //{
+            //    assert.SkipProperty(m => m.UseDefinedPageLayouts, "UseDefinedPageLayouts is NULL. Skipping");
+            //    assert.SkipProperty(m => m.DefinedPageLayouts, "DefinedPageLayouts as UseDefinedPageLayouts is NULL. Skipping");
+            //}
+
+            //// default page layout
+            //if (definition.InheritDefaultPageLayout.HasValue)
+            //    assert.ShouldBeEqual(m => m.InheritDefaultPageLayout, o => o.IsInheritingAvailablePageLayouts);
+            //else
+            //    assert.SkipProperty(m => m.InheritDefaultPageLayout, "InheritDefaultPageLayout is NULL. Skipping");
+
+            //if (definition.UseDefinedDefaultPageLayout.HasValue)
+            //{
+            //    assert.SkipProperty(m => m.UseDefinedDefaultPageLayout, "UseDefinedDefaultPageLayout is not NULL. Validating DefinedDefaultPageLayout.");
+
+            //    assert.ShouldBeEqual((p, s, d) =>
+            //    {
+            //        var srcProp = s.GetExpressionValue(m => m.DefinedDefaultPageLayout);
+            //        var isValid = true;
+
+            //        var currentPageLayout = spObject.DefaultPageLayout;
+
+            //        isValid = currentPageLayout.Name.ToUpper() == definition.DefinedDefaultPageLayout.ToUpper();
+
+            //        return new PropertyValidationResult
+            //        {
+            //            Tag = p.Tag,
+            //            Src = srcProp,
+            //            Dst = null,
+            //            IsValid = isValid
+            //        };
+            //    });
+
+            //}
+            //else
+            //{
+            //    assert.SkipProperty(m => m.UseDefinedDefaultPageLayout, "UseDefinedDefaultPageLayout is NULL. Skipping");
+            //    assert.SkipProperty(m => m.DefinedDefaultPageLayout, "DefinedDefaultPageLayout as UseDefinedDefaultPageLayout is NULL. Skipping");
+            //}
+
+            //assert.SkipProperty(m => m.ResetAllSubsitesToInheritDefaultPageLayout, "ResetAllSubsitesToInheritDefaultPageLayout can't be validates. Skipping.");
+
+            //// space settings
+            //if (definition.ConverBlankSpacesIntoHyphen.HasValue)
+            //    assert.ShouldBeEqual(m => m.ConverBlankSpacesIntoHyphen, o => o.GetConverBlankSpacesIntoHyphen());
+            //else
+            //    assert.SkipProperty(m => m.ConverBlankSpacesIntoHyphen, "ConverBlankSpacesIntoHyphen is NULL. Skipping");
+        }
+
+        #endregion
+    }
+
+    public static class PublishingWebExtensions
+    {
+
+
+        public static bool GetInheritWebTemplates(this PublishingWeb publishingWeb)
+        {
+            //return publishingWeb.IsInheritingAvailableWebTemplates;
+            throw new NotImplementedException();
+        }
+
+        public static bool GetConverBlankSpacesIntoHyphen(this PublishingWeb publishingWeb)
+        {
+            throw new NotImplementedException();
+
+            //var web = publishingWeb.Web;
+
+            //var key = "__AllowSpacesInNewPageName";
+
+            //if (!web.AllProperties.ContainsKey(key))
+            //    return false;
+
+            //return ConvertUtils.ToBool(web.AllProperties[key]).Value;
+        }
+    }
+}
