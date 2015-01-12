a2a4c8ab6d0b065f2519175f470a605c291bb696
SubPointSupport <support@subpointsolutions.com>
2015-01-11 23:03:57 -0800

    Add 'StandardXXXProvisionService' for SSOM/CSOM #292

---
+++ SPMeta2/SPMeta2.CSOM.Standard/Services/StandardProvisionService.cs
@@ -0,0 +1,17 @@
+﻿using SPMeta2.CSOM.Services;
+using SPMeta2.CSOM.Standard.ModelHandlers.Fields;
+
+namespace SPMeta2.CSOM.Standard.Services
+{
+    public class StandardSSOMProvisionService : CSOMProvisionService
+    {
+        #region constructors
+
+        public StandardSSOMProvisionService()
+        {
+            RegisterModelHandlers(typeof(TaxonomyFieldModelHandler).Assembly);
+        }
+
+        #endregion
+    }
+}
---
+++ SPMeta2/SPMeta2.SSOM.Standard/Services/StandardProvisionService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMeta2.SSOM.Services;
+using SPMeta2.SSOM.Standard.ModelHandlers.Fields;
+
+namespace SPMeta2.SSOM.Standard.Services
+{
+    public class StandardSSOMProvisionService : SSOMProvisionService
+    {
+        #region constructors
+
+        public StandardSSOMProvisionService()
+        {
+            RegisterModelHandlers(typeof(TaxonomyFieldModelHandler).Assembly);
+        }
+
+        #endregion
+    }
+}
