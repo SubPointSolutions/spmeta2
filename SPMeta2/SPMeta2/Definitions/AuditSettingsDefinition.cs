390b57f6714c7dfdc317467e38e6d63a19c46f12
SubPointSupport <support@subpointsolutions.com>
2015-01-19 01:07:40 -0800

    Add strong name for all assembiles #293

---
+++ SPMeta2/SPMeta2/Definitions/AuditSettingsDefinition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMeta2.Attributes;
+using SPMeta2.Attributes.Regression;
+
+namespace SPMeta2.Definitions
+{
+    /// <summary>
+    /// Allows to define and deploy auditing setting for sites, lists and items.
+    /// </summary>
+    [SPObjectType(SPObjectModelType.SSOM, "Microsoft.SharePoint.SPAudit", "Microsoft.SharePoint")]
+
+    [DefaultRootHostAttribute(typeof(WebDefinition))]
+    [DefaultParentHost(typeof(ListDefinition))]
+
+    [Serializable]
+
+    public class AuditSettingsDefinition : DefinitionBase
+    {
+        #region constructors
+
+        public AuditSettingsDefinition()
+        {
+
+        }
+
+        #endregion
+
+        #region properties
+
+        public Collection<string> AuditFlags { get; set; }
+
+        #endregion
+    }
+}
---
+++ SPMeta2/SPMeta2/Enumerations/BuiltInAuditMaskType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPMeta2.Enumerations
+{
+    public class BuiltInAuditMaskType
+    {
+        public static string None = "None";
+        public static string CheckOut = "CheckOut";
+        public static string CheckIn = "CheckIn";
+        public static string View = "View";
+        public static string Delete = "Delete";
+        public static string Update = "Update";
+        public static string ProfileChange = "ProfileChange";
+        public static string ChildDelete = "ChildDelete";
+        public static string SchemaChange = "SchemaChange";
+        public static string SecurityChange = "SecurityChange";
+        public static string Undelete = "Undelete";
+        public static string Workflow = "Workflow";
+        public static string Copy = "Copy";
+        public static string Move = "Move";
+        public static string Search = "Search";
+        public static string All = "All";
+    }
+}
