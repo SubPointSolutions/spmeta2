75e658211163f31dbc698de4e1d299b648880b00
SubPointSupport <support@subpointsolutions.com>
2015-01-19 01:43:54 -0800

    Add 'Nintex Workflow Constants' provision support #262

---
+++ SPMeta2/SPMeta2.Nintex/Enumerations/BuiltInWorkflowConstantType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPMeta2.Nintex.Enumerations
+{
+    public static class BuiltInWorkflowConstantType
+    {
+        #region properties
+
+        public static string Number = "Number";
+        public static string String = "String";
+        public static string Date = "Date";
+        public static string Credential = "Credential";
+        public static string SecureString = "SecureString";
+
+        #endregion
+
+    }
+}
