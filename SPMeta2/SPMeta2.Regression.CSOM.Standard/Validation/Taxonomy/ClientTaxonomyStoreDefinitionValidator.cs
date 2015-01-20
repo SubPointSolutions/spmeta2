a709e39b1496bf49616992b1bf663dd5df95ee1a
SubPointSupport <support@subpointsolutions.com>
2015-01-20 14:58:47 -0800

    Passed Regression.Scenarios.Taxonomy.TermStore tests for CSOM.

--- SPMeta2/SPMeta2.Regression.CSOM.Standard/Validation/Taxonomy/ClientTaxonomyStoreDefinitionValidator.cs
+++ SPMeta2/SPMeta2.Regression.CSOM.Standard/Validation/Taxonomy/ClientTaxonomyStoreDefinitionValidator.cs
@@ -1,4 +1,6 @@
-﻿using SPMeta2.CSOM.ModelHosts;
+﻿using Microsoft.SharePoint.Client.Taxonomy;
+using SPMeta2.Containers.Assertion;
+using SPMeta2.CSOM.ModelHosts;
 using SPMeta2.CSOM.Standard.ModelHandlers.Taxonomy;
 using SPMeta2.Definitions;
 using SPMeta2.Definitions.Base;
@@ -14,13 +16,52 @@ namespace SPMeta2.Regression.CSOM.Standard.Validation.Taxonomy
             var siteModelHost = modelHost.WithAssertAndCast<SiteModelHost>("modelHost", value => value.RequireNotNull());
             var definition = model.WithAssertAndCast<TaxonomyTermStoreDefinition>("model", value => value.RequireNotNull());
 
+            var site = siteModelHost.HostSite;
             var spObject = FindTermStore(siteModelHost, definition);
 
             var assert = ServiceFactory.AssertService
                            .NewAssert(definition, spObject)
                                  .ShouldNotBeNull(spObject);
 
-            // TODO
+            if (definition.Id.HasValue)
+                assert.ShouldBeEqual(m => m.Id, o => o.Id);
+            else
+                assert.SkipProperty(m => m.Id, "ID is NULL. Skipping.");
+
+            if (!string.IsNullOrEmpty(definition.Name))
+                assert.ShouldBeEqual(m => m.Name, o => o.Name);
+            else
+                assert.SkipProperty(m => m.Name, "Name is NULL. Skipping.");
+
+            if (definition.UseDefaultSiteCollectionTermStore.HasValue)
+            {
+                assert.ShouldBeEqual((p, s, d) =>
+                {
+                    var srcProp = s.GetExpressionValue(m => m.UseDefaultSiteCollectionTermStore);
+                    var session = TaxonomySession.GetTaxonomySession(siteModelHost.HostClientContext);
+
+                    var client = siteModelHost.HostClientContext;
+                    var termStore = session.GetDefaultSiteCollectionTermStore();
+
+                    client.Load(termStore, t => t.Id);
+                    client.Load(termStore, t => t.Name);
+
+                    siteModelHost.HostClientContext.ExecuteQuery();
+
+                    var isValid = termStore.Id == spObject.Id;
+
+                    return new PropertyValidationResult
+                    {
+                        Tag = p.Tag,
+                        Src = srcProp,
+                        Dst = null,
+                        IsValid = isValid
+                    };
+                });
+            }
+            else
+                assert.SkipProperty(m => m.UseDefaultSiteCollectionTermStore, "UseDefaultSiteCollectionTermStore is NULL. Skipping.");
+
 
         }
     }
