1e1b9e63c8b031317800707ebc31aaa0356c818b
SubPointSupport <support@subpointsolutions.com>
2015-01-26 15:25:40 -0800

    added Regression.Definitions.TaxonomyTermSetDefinition

---
+++ SPMeta2/SPMeta2.Regression.Tests/Impl/Definitions/TaxonomyTermSetDefinitionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.Attributes.Regression;
+using SPMeta2.Definitions;
+using SPMeta2.Models;
+using SPMeta2.Standard.Definitions.Fields;
+using SPMeta2.Standard.Definitions.Taxonomy;
+using SPMeta2.Syntax.Default;
+using SPMeta2.Utils;
+
+namespace SPMeta2.Regression.Tests.Impl.Definitions
+{
+    [TestClass]
+    public class TaxonomyTermSetDefinitionTests
+    {
+        #region properties
+
+        [TestMethod]
+        [TestCategory("Regression.Definitions.TaxonomyTermSetDefinition")]
+        public void TaxonomyTermSetDefinition_ShouldHave_IsAvailableForTagging_As_True()
+        {
+            var def = new TaxonomyTermSetDefinition();
+
+            Assert.IsTrue(def.IsAvailableForTagging);
+        }
+
+        #endregion
+    }
+}
