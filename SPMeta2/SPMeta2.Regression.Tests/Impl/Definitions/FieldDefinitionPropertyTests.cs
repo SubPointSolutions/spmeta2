e0fb5901edb318e817de7bf44ecdfb77d7257461
support@subpointsolutions.com <sbhsbhu8wxvKQgi>
2015-03-02 00:45:27 +1100

    Added FieldDefinitionPropertyTests

---
+++ SPMeta2/SPMeta2.Regression.Tests/Impl/Definitions/FieldDefinitionPropertyTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.Attributes.Regression;
+using SPMeta2.Definitions;
+using SPMeta2.Definitions.Fields;
+using SPMeta2.Models;
+using SPMeta2.Standard.Definitions.Fields;
+using SPMeta2.Standard.Definitions.Taxonomy;
+using SPMeta2.Syntax.Default;
+using SPMeta2.Utils;
+using SPMeta2.Regression.Tests.Base;
+
+namespace SPMeta2.Regression.Tests.Impl.Definitions
+{
+    [TestClass]
+    public class FieldDefinitionPropertyTests : SPMeta2RegresionTestBase
+    {
+        #region common
+
+        [ClassInitializeAttribute]
+        public static void Init(TestContext context)
+        {
+            InternalInit();
+        }
+
+        [ClassCleanupAttribute]
+        public static void Cleanup()
+        {
+            InternalCleanup();
+        }
+
+        #endregion
+
+        #region properties
+
+        [TestMethod]
+        [TestCategory("Regression.Definitions.FieldDefinitions.Properties")]
+        public void FieldDefinitions_ShouldHave_Correct_Indexed_Property()
+        {
+            var fieldDefinitionTypes = new List<Type>();
+
+            fieldDefinitionTypes.AddRange(ReflectionUtils.GetTypesFromAssembly<FieldDefinition>(typeof(FieldDefinition).Assembly));
+            fieldDefinitionTypes.AddRange(ReflectionUtils.GetTypesFromAssembly<FieldDefinition>(typeof(TaxonomyFieldDefinition).Assembly));
+
+            foreach (var fieldDefintion in fieldDefinitionTypes)
+            {
+                Trace.WriteLine(string.Format("Checking Indexed propr for Indexed def:[{0}]", fieldDefintion.GetType().Name));
+
+                var indexedSiteModel = SPMeta2Model.NewSiteModel(m => { });
+                var indexedSiteField = ModelGeneratorService.GetRandomDefinition(fieldDefintion) as FieldDefinition;
+
+                indexedSiteModel.AddField(indexedSiteField);
+                indexedSiteField.Indexed = true;
+                TestModel(indexedSiteModel);
+
+                Trace.WriteLine(string.Format("Checking Indexed propr for non-Indexed def:[{0}]", fieldDefintion.GetType().Name));
+
+                var nonIdexedSiteModel = SPMeta2Model.NewSiteModel(m => { });
+                var nonIndexedSiteField = ModelGeneratorService.GetRandomDefinition(fieldDefintion) as FieldDefinition;
+
+                nonIdexedSiteModel.AddField(nonIndexedSiteField);
+                nonIndexedSiteField.Indexed = false;
+                TestModel(nonIdexedSiteModel);
+            }
+        }
+
+        #endregion
+    }
+}
