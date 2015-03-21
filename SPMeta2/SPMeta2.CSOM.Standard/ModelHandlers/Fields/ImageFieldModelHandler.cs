2376e292bd4492e42b576694df79511235b98dc9
support@subpointsolutions.com <sbhsbhu8wxvKQgi>
2015-03-22 05:28:26 +1100

    CSOM impl for Image, Link and HTML publishing fields

---
+++ SPMeta2/SPMeta2.CSOM.Standard/ModelHandlers/Fields/ImageFieldModelHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Xml.Linq;
+using Microsoft.SharePoint.Client;
+using SPMeta2.CSOM.ModelHandlers;
+using SPMeta2.CSOM.ModelHandlers.Fields;
+using SPMeta2.Definitions;
+using SPMeta2.Definitions.Fields;
+using SPMeta2.Enumerations;
+using SPMeta2.Standard.Definitions.Fields;
+using SPMeta2.Utils;
+
+namespace SPMeta2.CSOM.Standard.ModelHandlers.Fields
+{
+    public class ImageFieldModelHandler : FieldModelHandler
+    {
+        #region properties
+
+        public override Type TargetType
+        {
+            get { return typeof(ImageFieldDefinition); }
+        }
+
+        //protected override Type GetTargetFieldType(FieldDefinition model)
+        //{
+        //    return typeof(FieldChoice);
+        //}
+
+        #endregion
+
+        #region methods
+
+        protected override void ProcessFieldProperties(Field field, FieldDefinition fieldModel)
+        {
+            // let base setting be setup
+            base.ProcessFieldProperties(field, fieldModel);
+
+            //if (!string.IsNullOrEmpty(fieldModel.ValidationMessage))
+            //    field.ValidationMessage = fieldModel.ValidationMessage;
+
+            //if (!string.IsNullOrEmpty(fieldModel.ValidationFormula))
+            //    field.ValidationFormula = fieldModel.ValidationFormula;
+        }
+
+        protected override void ProcessSPFieldXElement(XElement fieldTemplate, FieldDefinition fieldModel)
+        {
+            base.ProcessSPFieldXElement(fieldTemplate, fieldModel);
+
+            var typedFieldModel = fieldModel.WithAssertAndCast<ImageFieldDefinition>("model", value => value.RequireNotNull());
+
+            //fieldTemplate.SetAttribute(BuiltInFieldAttributes.Format, typedFieldModel.EditFormat);
+        }
+
+        #endregion
+    }
+}
