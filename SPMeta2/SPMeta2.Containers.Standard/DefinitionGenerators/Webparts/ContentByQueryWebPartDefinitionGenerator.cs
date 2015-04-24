f80b185b76635fce236152ad0c13b3187befe03e
SubPointSupport <support@subpointsolutions.com>
2015-04-24 02:30:39 -0700

    missing generator

---
+++ SPMeta2/SPMeta2.Containers.Standard/DefinitionGenerators/Webparts/ContentByQueryWebPartDefinitionGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SPMeta2.Containers.Services.Base;
+using SPMeta2.Definitions;
+using SPMeta2.Standard.Definitions.Webparts;
+
+namespace SPMeta2.Containers.Standard.DefinitionGenerators.Webparts
+{
+    public class ContentBySearchWebPartDefinitionGenerator : TypedDefinitionGeneratorServiceBase<ContentBySearchWebPartDefinition>
+    {
+        public override DefinitionBase GenerateRandomDefinition(Action<DefinitionBase> action)
+        {
+            return WithEmptyDefinition(def =>
+            {
+                def.Id = Rnd.String();
+                def.Title = Rnd.String();
+
+                def.ZoneId = "FullPage";
+                def.ZoneIndex = Rnd.Int(100);
+
+                // TODO
+
+
+            });
+        }
+    }
+}
