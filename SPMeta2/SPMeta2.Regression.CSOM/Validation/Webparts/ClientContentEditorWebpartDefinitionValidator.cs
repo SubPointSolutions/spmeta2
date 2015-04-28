49faddff8327107b973c3cb9547bdff187974438
support@subpointsolutions.com <sbhsbhu8wxvKQgi>
2015-04-29 02:20:07 +1000

    CSOM, passed CanDeploy_ContentEditorWebPart_WithContentLink_With_Tokens() and CanDeployRandom_ContentEditorWebPartDefinition() tests, Implement CSOM related validation on webpart definitions #474

--- SPMeta2/SPMeta2.Regression.CSOM/Validation/Webparts/ClientContentEditorWebpartDefinitionValidator.cs
+++ SPMeta2/SPMeta2.Regression.CSOM/Validation/Webparts/ClientContentEditorWebpartDefinitionValidator.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using SPMeta2.Containers.Assertion;
 using SPMeta2.CSOM.ModelHandlers;
 using SPMeta2.CSOM.ModelHandlers.Webparts;
 using SPMeta2.CSOM.ModelHosts;
 using SPMeta2.Definitions;
 using SPMeta2.Definitions.Base;
 using SPMeta2.Definitions.Webparts;
+using SPMeta2.Services;
 using SPMeta2.Utils;
 
 namespace SPMeta2.Regression.CSOM.Validation.Webparts
@@ -30,6 +32,55 @@ namespace SPMeta2.Regression.CSOM.Validation.Webparts
                 var assert = ServiceFactory.AssertService
                                            .NewAssert(model, definition, spObject)
                                                  .ShouldNotBeNull(spObject);
+
+
+                if (!string.IsNullOrEmpty(definition.Content))
+                {
+                    var value = CurrentWebPartXml.GetContentEditorWebPartProperty("Content");
+
+                    assert.ShouldBeEqual((p, s, d) =>
+                    {
+                        var srcProp = s.GetExpressionValue(m => m.Content);
+                        var isValid = definition.Content == value;
+
+                        return new PropertyValidationResult
+                        {
+                            Tag = p.Tag,
+                            Src = srcProp,
+                            Dst = null,
+                            IsValid = isValid
+                        };
+                    });
+                }
+                else
+                    assert.SkipProperty(m => m.Content, "Content is null or empty. Skipping.");
+
+                if (!string.IsNullOrEmpty(definition.ContentLink))
+                {
+                    var value = CurrentWebPartXml.GetContentEditorWebPartProperty("ContentLink");
+                    var defValue = TokenReplacementService.ReplaceTokens(new TokenReplacementContext
+                    {
+                        Context = listItemModelHost.HostClientContext,
+                        Value = definition.ContentLink
+                    }).Value;
+
+                    var isValid = defValue.ToUpper() == value.ToUpper();
+
+                    assert.ShouldBeEqual((p, s, d) =>
+                    {
+                        var srcProp = s.GetExpressionValue(m => m.ContentLink);
+
+                        return new PropertyValidationResult
+                        {
+                            Tag = p.Tag,
+                            Src = srcProp,
+                            Dst = null,
+                            IsValid = isValid
+                        };
+                    });
+                }
+                else
+                    assert.SkipProperty(m => m.ContentLink, "ContentLink is null or empty. Skipping.");
 
             });
         }
