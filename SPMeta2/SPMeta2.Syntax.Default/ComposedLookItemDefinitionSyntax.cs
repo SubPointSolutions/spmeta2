89427f4d9423667a1f2145a7ab554cfee495fb54
unknown <kh@plumsail.com>
2015-05-21 17:02:02 +0300

    Bug fixes for composed look provision. Syntax for composed look provision was added.

---
+++ SPMeta2/SPMeta2.Syntax.Default/ComposedLookItemDefinitionSyntax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SPMeta2.Definitions;
+using SPMeta2.Models;
+using SPMeta2.Syntax.Default.Extensions;
+
+namespace SPMeta2.Syntax.Default
+{
+    public static class ComposedLookItemDefinitionSyntax
+    {
+        #region methods
+
+        public static ModelNode AddComposedLookItem(this ModelNode model, ComposedLookItemDefinition definition)
+        {
+            return AddComposedLookItem(model, definition, null);
+        }
+
+        public static ModelNode AddComposedLookItem(this ModelNode model, ComposedLookItemDefinition definition, Action<ModelNode> action)
+        {
+            return model.AddDefinitionNode(definition, action);
+        }
+
+        #endregion
+    }
+}
