cb213b4272216ee57f210db6324d5b0df7011c03
support@subpointsolutions.com <support@subpointsolutions.com>
2015-07-01 12:12:04 +1000

    Implement fault tolerant provision for CSOM #567, DefaultClientRuntimeContextService impl,  fault tolerant  is still TODO

---
+++ SPMeta2/SPMeta2.CSOM/Services/Impl/DefaultClientRuntimeContextService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace SPMeta2.CSOM.Services.Impl
+{
+    public class DefaultClientRuntimeContextService : ClientRuntimeContextServiceBase
+    {
+        public override void ExecuteQuery(ClientRuntimeContext context)
+        {
+            context.ExecuteQuery();
+        }
+    }
+}
