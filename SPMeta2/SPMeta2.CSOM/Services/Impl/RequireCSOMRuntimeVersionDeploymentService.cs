eeed0154b57abf9eb91529c2106feafbb0834622
support@subpointsolutions.com <support@subpointsolutions.com>
2015-09-01 00:48:33 +1000

    SPMeta2 1.2.15-beta1 release

--- SPMeta2/SPMeta2.CSOM/Services/Impl/RequireCSOMRuntimeVersionDeploymentService.cs
+++ SPMeta2/SPMeta2.CSOM/Services/Impl/RequireCSOMRuntimeVersionDeploymentService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 using Microsoft.SharePoint.Client;
 using SPMeta2.Exceptions;
 using SPMeta2.ModelHosts;
