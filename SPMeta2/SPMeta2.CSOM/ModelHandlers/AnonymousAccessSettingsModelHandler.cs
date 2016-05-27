414f2c6995a89d960416a68b6d74216a8487e526
support@subpointsolutions.com <support@subpointsolutions.com>
2016-05-27 06:15:42 -0700

    + Implement CSOM handler for AnonymousAccessSettingsDefinition #841

---
+++ SPMeta2/SPMeta2.CSOM/ModelHandlers/AnonymousAccessSettingsModelHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.SharePoint;
+using SPMeta2.Common;
+using SPMeta2.Definitions;
+using SPMeta2.Services;
+using SPMeta2.Utils;
+using SPMeta2.CSOM.ModelHandlers;
+using SPMeta2.CSOM.ModelHosts;
+using Microsoft.SharePoint.Client;
+
+namespace SPMeta2.CSOM.ModelHandlers
+{
+    public class AnonymousAccessSettingsModelHandler : CSOMModelHandlerBase
+    {
+
+        public override Type TargetType
+        {
+            get { return typeof(AnonymousAccessSettingsDefinition); }
+        }
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var webModelHost = modelHost.WithAssertAndCast<WebModelHost>("modelHost", value => value.RequireNotNull());
+            var definition = model.WithAssertAndCast<AnonymousAccessSettingsDefinition>("model", value => value.RequireNotNull());
+
+            var web = webModelHost.HostWeb;
+
+            TraceService.Warning((int)LogEventId.ModelProvisionCoreCall, "AnonymousAccessSettingsDefinition is not supported by CSOM API. Doing nothing.");
+
+            InvokeOnModelEvent(this, new ModelEventArgs
+            {
+                CurrentModelNode = null,
+                Model = null,
+                EventType = ModelEventType.OnProvisioning,
+                Object = web,
+                ObjectType = typeof(Web),
+                ObjectDefinition = definition,
+                ModelHost = modelHost
+            });
+
+            // port from SSOM
+            // just do notihng, so that the provision would not fail
+
+
+
+            //if (!web.HasUniqueRoleAssignments)
+            //{
+            //    TraceService.Information((int)LogEventId.ModelProvisionCoreCall,
+            //        "web.HasUniqueRoleAssignments = false. Breaking with false-false options.");
+            //    web.BreakRoleInheritance(false, false);
+            //}
+
+            //var anonState = (SPWeb.WebAnonymousState)Enum.Parse(typeof(SPWeb.WebAnonymousState), definition.AnonymousState);
+            //web.AnonymousState = anonState;
+
+            //if (definition.AnonymousPermMask64.Any())
+            //{
+            //    var permissions = SPBasePermissions.EmptyMask;
+
+            //    foreach (var permissionString in definition.AnonymousPermMask64)
+            //        permissions = permissions | (SPBasePermissions)Enum.Parse(typeof(SPBasePermissions), permissionString);
+
+            //    web.AnonymousPermMask64 = permissions;
+            //}
+
+            InvokeOnModelEvent(this, new ModelEventArgs
+            {
+                CurrentModelNode = null,
+                Model = null,
+                EventType = ModelEventType.OnProvisioned,
+                Object = web,
+                ObjectType = typeof(Web),
+                ObjectDefinition = definition,
+                ModelHost = modelHost
+            });
+
+            web.Update();
+        }
+    }
+}
---
+++ SPMeta2/SPMeta2.Regression.CSOM/Validation/ClientAnonymousAccessSettingsDefinitionValidator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.SharePoint;
+using SPMeta2.Containers.Assertion;
+using SPMeta2.Definitions;
+using SPMeta2.Definitions.Base;
+using SPMeta2.Enumerations;
+using SPMeta2.Utils;
+using SPMeta2.CSOM.ModelHandlers;
+using SPMeta2.CSOM.ModelHosts;
+
+
+namespace SPMeta2.Regression.CSOM.Validation
+{
+    public class AnonymousAccessSettingsDefinitionValidator : AnonymousAccessSettingsModelHandler
+    {
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var webModelHost = modelHost.WithAssertAndCast<WebModelHost>("modelHost", value => value.RequireNotNull());
+            var definition = model.WithAssertAndCast<AnonymousAccessSettingsDefinition>("model",
+                value => value.RequireNotNull());
+
+            var spObject = webModelHost.HostWeb;
+
+            var assert = ServiceFactory.AssertService
+                .NewAssert(definition, spObject)
+                .ShouldNotBeNull(spObject);
+
+            assert.SkipProperty(m => m.AnonymousPermMask64, "Skipping due to CSOM API limitation.");
+            assert.SkipProperty(m => m.AnonymousState, "Skipping due to CSOM API limitation.");
+
+            //SSOm port
+
+            //if (definition.AnonymousState == BuiltInWebAnonymousState.Disabled)
+            //{
+            //    assert.SkipProperty(m => m.AnonymousPermMask64, "AnonymousState is disables");
+            //}
+            //else
+            //{
+            //    assert.ShouldBeEqual((p, s, d) =>
+            //    {
+            //        var srcProp = s.GetExpressionValue(def => def.AnonymousPermMask64);
+            //        var dstProp = d.GetExpressionValue(ct => ct.AnonymousPermMask64);
+
+            //        var hasCorrectRights = true;
+
+            //        foreach (var srcRight in s.AnonymousPermMask64)
+            //        {
+            //            var srcPermission = (SPBasePermissions)Enum.Parse(typeof(SPBasePermissions), srcRight);
+
+            //            var tmpRight = d.AnonymousPermMask64.HasFlag(srcPermission);
+
+            //            if (tmpRight == false)
+            //                hasCorrectRights = false;
+            //        }
+
+            //        return new PropertyValidationResult
+            //        {
+            //            Tag = p.Tag,
+            //            Src = srcProp,
+            //            Dst = dstProp,
+            //            IsValid = hasCorrectRights
+            //        };
+            //    });
+            //}
+
+            //assert.ShouldBeEqual(m => m.AnonymousState, o => o.AnonymousState.ToString());
+        }
+    }
+}
