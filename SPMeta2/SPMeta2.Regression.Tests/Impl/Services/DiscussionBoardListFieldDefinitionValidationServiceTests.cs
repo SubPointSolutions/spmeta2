6fbe7bb5863167f789b356ec1490713bd6eba6dd
support@subpointsolutions.com <support@subpointsolutions.com>
2016-09-06 03:15:22 -0700

    + Enhance ListDefinition provision - add discussion board validation #879

---
+++ SPMeta2/SPMeta2.Regression.Tests/Impl/Services/DiscussionBoardListFieldDefinitionValidationServiceTests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.BuiltInDefinitions;
+using SPMeta2.Containers;
+using SPMeta2.Containers.Services;
+using SPMeta2.Definitions;
+using SPMeta2.Enumerations;
+using SPMeta2.Exceptions;
+using SPMeta2.Regression.Tests.Base;
+using SPMeta2.Services.Impl;
+using SPMeta2.Syntax.Default;
+using SPMeta2.Regression.Tests.Impl.Scenarios.Base;
+using SPMeta2.Services.Impl.Validation;
+
+namespace SPMeta2.Regression.Tests.Impl.Services
+{
+    [TestClass]
+    public class DiscussionBoardListFieldDefinitionValidationServiceTests : SPMeta2DefinitionRegresionTestBase
+    {
+        #region constructors
+
+        public DiscussionBoardListFieldDefinitionValidationServiceTests()
+        {
+            Service = new DiscussionBoardListFieldDefinitionValidationService();
+        }
+
+        #endregion
+
+        #region properties
+
+        public DiscussionBoardListFieldDefinitionValidationService Service { get; set; }
+
+        #endregion
+
+
+        #region tests
+
+        [TestMethod]
+        [TestCategory("Regression.Services.DiscussionBoardListFieldDefinitionValidationService")]
+        [TestCategory("CI.Core")]
+        public void ShouldPass_On_Non_DiscussionBoardList()
+        {
+            var model = SPMeta2Model.NewWebModel(web =>
+            {
+                web.AddRandomList();
+                web.AddRandomList();
+            });
+
+            Service.DeployModel(null, model);
+        }
+
+        [TestMethod]
+        [TestCategory("Regression.Services.DiscussionBoardListFieldDefinitionValidationService")]
+        [TestCategory("CI.Core")]
+        public void ShouldFail_On_DiscussionBoardList_WithoutContenTypes_ByTemplateType()
+        {
+            var isValid = false;
+
+            var listDef = ModelGeneratorService.GetRandomDefinition<ListDefinition>(list =>
+            {
+                list.TemplateType = BuiltInListTemplateTypeId.DiscussionBoard;
+                list.ContentTypesEnabled = false;
+            });
+
+            var model = SPMeta2Model.NewWebModel(web =>
+            {
+                web.AddList(listDef);
+            });
+
+            try
+            {
+                Service.DeployModel(null, model);
+            }
+            catch (Exception e)
+            {
+                isValid = IsCorrectValidationException(e);
+            }
+
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        [TestCategory("Regression.Services.DiscussionBoardListFieldDefinitionValidationService")]
+        [TestCategory("CI.Core")]
+        public void ShouldFail_On_DiscussionBoardList_WithoutContenTypes_ByTemplateName()
+        {
+            var isValid = false;
+
+            var listDef = ModelGeneratorService.GetRandomDefinition<ListDefinition>(list =>
+            {
+                list.TemplateName = BuiltInListTemplates.DiscussionBoard.InternalName;
+                list.ContentTypesEnabled = false;
+            });
+
+            var model = SPMeta2Model.NewWebModel(web =>
+            {
+                web.AddList(listDef);
+            });
+
+            try
+            {
+                Service.DeployModel(null, model);
+            }
+            catch (Exception e)
+            {
+                isValid = IsCorrectValidationException(e);
+            }
+
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        [TestCategory("Regression.Services.DiscussionBoardListFieldDefinitionValidationService")]
+        [TestCategory("CI.Core")]
+        public void ShouldPass_On_DiscussionBoardList_WithoutContenTypes_ByTemplateType()
+        {
+            var listDef = ModelGeneratorService.GetRandomDefinition<ListDefinition>(list =>
+            {
+                list.TemplateType = BuiltInListTemplateTypeId.DiscussionBoard;
+                list.ContentTypesEnabled = true;
+            });
+
+            var model = SPMeta2Model.NewWebModel(web =>
+            {
+                web.AddList(listDef);
+            });
+
+            Service.DeployModel(null, model);
+        }
+
+        [TestMethod]
+        [TestCategory("Regression.Services.DiscussionBoardListFieldDefinitionValidationService")]
+        [TestCategory("CI.Core")]
+        public void ShouldPass_On_DiscussionBoardList_WithoutContenTypes_ByTemplateName()
+        {
+            var listDef = ModelGeneratorService.GetRandomDefinition<ListDefinition>(list =>
+            {
+                list.TemplateName = BuiltInListTemplates.DiscussionBoard.InternalName;
+                list.ContentTypesEnabled = true;
+            });
+
+            var model = SPMeta2Model.NewWebModel(web =>
+            {
+                web.AddList(listDef);
+            });
+
+            Service.DeployModel(null, model);
+        }
+
+        #endregion
+    }
+}
---
+++ SPMeta2/SPMeta2/Services/ServiceModelHandlers/DiscussionBoardListDefinitionValidationModelHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using SPMeta2.Attributes.Capabilities;
+using SPMeta2.Definitions;
+using SPMeta2.Definitions.Fields;
+using SPMeta2.Enumerations;
+using SPMeta2.Exceptions;
+using SPMeta2.Syntax.Default;
+using SPMeta2.Utils;
+
+namespace SPMeta2.Services.ServiceModelHandlers
+{
+    public class DiscussionBoardListDefinitionValidationModelHandler :
+        TypedDefinitionModelHandlerBase<ListDefinition>
+    {
+        #region propeties
+
+        private static string ErrorMessage = "ListDefinition for discussion board must have content types enabled - https://github.com/SubPointSolutions/spmeta2/issues/879";
+
+        #endregion
+
+        #region methods
+
+        protected override void ProcessDefinition(object modelHost, ListDefinition model)
+        {
+            // Enhance ListDefinition provision - add discussion board validation #879
+            // https://github.com/SubPointSolutions/spmeta2/issues/879
+
+            // All discussion boards must have content types enabled
+            // https://github.com/SubPointSolutions/spmeta2/issues/879
+
+            if (model.TemplateType == BuiltInListTemplateTypeId.DiscussionBoard
+                && !model.ContentTypesEnabled)
+            {
+                throw new SPMeta2ModelValidationException(ErrorMessage);
+            }
+
+            if (model.TemplateName == BuiltInListTemplates.DiscussionBoard.InternalName
+                && !model.ContentTypesEnabled)
+            {
+                throw new SPMeta2ModelValidationException(ErrorMessage);
+            }
+        }
+
+        #endregion
+    }
+}
