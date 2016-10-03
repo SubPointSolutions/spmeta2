aa0f627f381cf99ff7c114362c6c21c29d290cd7
support@subpointsolutions.com <support@subpointsolutions.com>
2016-10-02 19:30:24 -0700

    + Enhance OutcomeChoiceField - add choices #898 + added Regression.Scenarios.Fields.OutcomeChoiceField tests + build fix

--- SPMeta2/SPMeta2.SSOM/ModelHandlers/Fields/OutcomeChoiceFieldModelHandler.cs
+++ SPMeta2/SPMeta2.SSOM/ModelHandlers/Fields/OutcomeChoiceFieldModelHandler.cs
@@ -8,7 +8,7 @@ using SPMeta2.Utils;
 
 namespace SPMeta2.SSOM.ModelHandlers.Fields
 {
-    public class OutcomeChoiceFieldModelHandler : FieldModelHandler
+    public class OutcomeChoiceFieldModelHandler : ChoiceFieldModelHandler
     {
         #region properties
 
