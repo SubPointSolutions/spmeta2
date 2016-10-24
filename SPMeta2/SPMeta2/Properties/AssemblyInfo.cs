2e7a5f0a17c6704786423b8020ceb1a8dc8bb414
support@subpointsolutions.com <support@subpointsolutions.com>
2016-10-24 00:04:32 -0700

    + WebNavigationSettingsDefinition - missed impl for AddNewPagesToNavigation/CreateFriendlyUrlsForNewPages props #906 + added CanDeploy_WebNavigationSettings_With_AddNewPages_And_FriendlyUrls test

--- SPMeta2/SPMeta2/Properties/AssemblyInfo.cs
+++ SPMeta2/SPMeta2/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.16286.0924")]
+[assembly: AssemblyFileVersion("1.2.16298.0654")]
