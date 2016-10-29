06e2f4f1679f9d8af1f2edc16275890677ebfdfd
support@subpointsolutions.com <support@subpointsolutions.com>
2016-10-28 23:11:43 -0700

    + NET35 build fix

--- SPMeta2/SPMeta2.Regression.Tests/Utils/X509Utils.cs
+++ SPMeta2/SPMeta2.Regression.Tests/Utils/X509Utils.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace SPMeta2.Regression.Tests.Utils
 {
