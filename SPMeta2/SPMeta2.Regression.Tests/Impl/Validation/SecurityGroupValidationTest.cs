05f3d8640ff30fd977ba1314a0ce9331b7f9561c
support@subpointsolutions.com <support@subpointsolutions.com>
2017-03-01 06:11:19 +0000

    + Validation Issue for Built In Security Groups #968 + added Validation_SecurityGroup_ShouldAllow_EmptyName_If_BuiltIn test

---
+++ SPMeta2/SPMeta2.Regression.Tests/Impl/Validation/SecurityGroupValidationTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.BuiltInDefinitions;
+using SPMeta2.Containers;
+using SPMeta2.Definitions;
+using SPMeta2.Enumerations;
+using SPMeta2.Models;
+using SPMeta2.Regression.Tests.Impl.Scenarios.Base;
+using SPMeta2.Standard.Definitions;
+using SPMeta2.Standard.Syntax;
+using SPMeta2.Syntax.Default;
+using System.Collections.Generic;
+
+namespace SPMeta2.Regression.Tests.Impl.Validation
+{
+    [TestClass]
+    public class SecurityGroupValidationTest : SPMeta2RegresionScenarioTestBase
+    {
+        #region internal
+
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            InternalInit();
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            InternalCleanup();
+        }
+
+        #endregion
+
+        #region group link options
+
+        [TestMethod]
+        [TestCategory("Regression.Validation.PublishingPageLayout")]
+        public void Validation_SecurityGroup_ShouldAllow_EmptyName_If_BuiltIn()
+        {
+            var builtInGroups = new List<SecurityGroupDefinition>()
+            {
+                BuiltInSecurityGroupDefinitions.AssociatedMemberGroup,
+                BuiltInSecurityGroupDefinitions.AssociatedOwnerGroup,
+                BuiltInSecurityGroupDefinitions.AssociatedVisitorsGroup
+            };
+
+            var siteModel = SPMeta2Model.NewSiteModel(site =>
+            {
+                site.AddSecurityGroups(builtInGroups);
+            });
+
+            TestModels(new ModelNode[] { siteModel });
+        }
+
+        #endregion
+    }
+}
